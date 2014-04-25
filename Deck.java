5ddf0ef9f611e38b0a6530edb4b448bfd7247478
nrex <nathanielrex@gmail.com>
2014-04-24 22:38:24 -0400

    Progress 5
    Still working on warRound

--- Deck.java
+++ Deck.java
@@ -13,7 +13,7 @@ public class Deck
    public void freshDeck()
    {
       deck = new ArrayList<Card>(CARDS_IN_DECK);
-      System.out.println(deck.size());
+      //System.out.println(deck.size());
 
       for (int r = Card.ACE; r<=Card.KING;r++)
       {
--- War.java
+++ War.java
@@ -6,7 +6,7 @@ public class War
    private ArrayList<Card> player;
    private ArrayList<Card> computer;
    private ArrayList<Card> warPile;
-   private int round_count; //a counter for info
+   public int round_count; //a counter for info
    private int HALF = 26;
    private Card temp_card;
 
@@ -58,8 +58,8 @@ public class War
       }
       else //prepare to start war
       {  
-         System.out.println("Putting: " + temp_player_card + " back in player");
-         System.out.println("Putting: " + temp_computer_card + " back in computer");
+         //System.out.println("Putting: " + temp_player_card + " back in player");
+         //System.out.println("Putting: " + temp_computer_card + " back in computer");
 
          player.add(0, temp_player_card);
          computer.add(0, temp_computer_card);
@@ -71,81 +71,64 @@ public class War
       
       public int warRound()
       {
+         System.out.println("War!");
+         Card warCard;
          Card temp_player_card = player.remove(0);
          Card temp_computer_card = computer.remove(0);
          warPile.add(temp_player_card); //put the current cards into the pile
          warPile.add(temp_computer_card); //put the current cards into the pile
          temp_player_card = player.remove(0); //put the next card into the pile
          temp_computer_card = computer.remove(0); //put the next card into the pile
-         warPile.add(temp_player_card);
+         warPile.add(temp_player_card); 
          warPile.add(temp_computer_card);
          
-         System.out.println(player.size());
-         System.out.println(computer.size());
-         System.out.println("WarPile Size: " +warPile.size());
+         //System.out.println(player.size());
+         //System.out.println(computer.size());
+         //System.out.println("WarPile Size: " +warPile.size());
          
          int war_winner = this.playRound();
-         
-         if (war_winner == 2) //if the player won war;
+         int warPileLength = warPile.size();
+         if (war_winner == 2)
          {
-            for (int i = 0; i < warPile.size();i++) //put all of the cards fro mthe pile into the payer's deck
-               {
-                  Card warCard = warPile.remove(0);
-                  player.add(warCard);
-               }//player won war
-         }
+            for (int i = 0; i < warPileLength;i++)
+            {
+               warCard = warPile.remove(0);
+               player.add(warCard);
+            }
+         }//player won war
+         
          else if (war_winner == 3)
          {
-            for (int i = 0; i < warPile.size();i++) //put all of the cards from the pile into the computer's deck
-               {
-                  Card warCard = warPile.remove(0);
-                  computer.add(warCard);
-               }//computer won war
-         }
+            for (int i = 0; i < warPileLength;i++)
+            {
+               warCard = warPile.remove(0);
+               computer.add(warCard);
+            }
+         }//computer won war
          else 
          {
-            this.playRound();
+            this.warRound();
+            war_winner = 0;
+            
          }//must play another round
          System.out.println(player.size());
          System.out.println(computer.size());
          return 0;
       }//warRound method
-        
- 
-   
-   public static void displayDeck(Deck d)
-   {
-      int i = 0;
-      while (!(d.isEmpty()))
-         System.out.println(i++ + " : " + d.dealCard().toString());
-   }
    
    public static void main(String[] args) throws NullPointerException
    {
       int blah = 1;
       War warGame = new War();
-      //System.out.println(warGame.warDeck.isEmpty());
-      //System.out.println(warGame.player.size());
-      //System.out.println(warGame.computer.size());
-      do
-      {
-         blah = warGame.playRound();
-      }
-      while(blah!=0);
-      System.out.println("Round: " + warGame.round_count);
-      //System.out.println(warGame.player.size());
-      //System.out.println(warGame.computer.size());
+//       do
+//       {
+//          blah = warGame.playRound();
+//       }
+//       while(blah!=0);
+      do {
+      warGame.warRound();
+      } while(warGame.round_count<2);
       
-   
-   
+      System.out.println("Round: " + warGame.round_count);
    }//main
-
-
-
-
-
-  
-
-
-
 }//public class 
