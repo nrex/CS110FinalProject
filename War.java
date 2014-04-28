1680824b63a1c8c4ace8571616db0a44ac36fef5
nrex <nathanielrex@gmail.com>
2014-04-27 23:42:53 -0400

    Progress 12
    Almost finished, just need to figure out how to handle a winner.

--- War.java
+++ War.java
@@ -10,6 +10,7 @@ public class War
    private int HALF = 26;
    private Card temp_card;
    private int war_var;
+   public int game_over = 0;
 
    
    public War() throws NullPointerException //constructor establishes the deck as well as both of the players
@@ -39,37 +40,53 @@ public class War
    public int playRound()//play a round of war
    {
       round_count+=1;//increase the round count
-      Card temp_player_card = player.remove(0); 
-      Card temp_computer_card = computer.remove(0);
-      System.out.println("P: "+temp_player_card);
-      System.out.println("C: "+temp_computer_card);
       System.out.println(player.size());
       System.out.println(computer.size());
-      
-      if (temp_player_card.getRank() > temp_computer_card.getRank())//player has higher card
-      {  
-         player.add(temp_player_card); //put the player's card back on the bottom
-         player.add(temp_computer_card); //player won, so put the computer's card on the bottom
-         System.out.println("Player Won");
-         return 2;
-      }
-      else if (temp_player_card.getRank() < temp_computer_card.getRank()) //computer has higher card
-      {
-         computer.add(temp_computer_card);
-         computer.add(temp_player_card);
-         System.out.println("Computer Won");
-         return 3;
-      }
-      else //prepare to start war
-      {  
-         //System.out.println("Putting: " + temp_player_card + " back in player");
-         //System.out.println("Putting: " + temp_computer_card + " back in computer");
+      Card temp_player_card = player.remove(0); 
+      Card temp_computer_card = computer.remove(0);
 
-         player.add(0, temp_player_card);
-         computer.add(0, temp_computer_card);
-         war_var = this.warRound();
-         return 0;
+      if (player.isEmpty())
+      {
+         game_over = 1;
+         System.out.println("Computer won the game!");
       }
+      else if (computer.isEmpty())
+      {
+         game_over = 1;
+         System.out.println("Player won the game!");
+      }
+      if (game_over == 0)
+      { 
+                  System.out.println("P: "+temp_player_card);
+         System.out.println("C: "+temp_computer_card);
+         
+         
+         if (temp_player_card.getRank() > temp_computer_card.getRank())//player has higher card
+         {  
+            player.add(temp_player_card); //put the player's card back on the bottom
+            player.add(temp_computer_card); //player won, so put the computer's card on the bottom
+            System.out.println("Player Won");
+            return 2;
+         }
+         else if (temp_player_card.getRank() < temp_computer_card.getRank()) //computer has higher card
+         {
+            computer.add(temp_computer_card);
+            computer.add(temp_player_card);
+            System.out.println("Computer Won");
+            return 3;
+         }
+         else //prepare to start war
+         {  
+            //System.out.println("Putting: " + temp_player_card + " back in player");
+            //System.out.println("Putting: " + temp_computer_card + " back in computer");
+   
+            player.add(0, temp_player_card);
+            computer.add(0, temp_computer_card);
+            war_var = this.warRound();
+            return 0;
+         }
+       }//game continues
+       return 5;
    }//end play round
      
       
@@ -79,75 +96,90 @@ public class War
          Card warCard;
          Card temp_player_card = player.remove(0);
          Card temp_computer_card = computer.remove(0);
+
          warPile.add(temp_player_card); //put the current cards into the pile
          warPile.add(temp_computer_card); //put the current cards into the pile
          temp_player_card = player.remove(0); //put the next card into the pile
          temp_computer_card = computer.remove(0); //put the next card into the pile
-         warPile.add(temp_player_card); 
-         warPile.add(temp_computer_card);
-         
-         //System.out.println(player.size());
-         //System.out.println(computer.size());
-         //System.out.println("WarPile Size: " +warPile.size());
-         
-         int war_winner = this.playRound(); //play a round to determine the winner.
-         
-         int warPileLength = warPile.size();
-         if (war_winner == 2)
+         if (player.isEmpty())
          {
-            for (int i = 0; i < warPileLength;i++)
-            {
-               warCard = warPile.remove(0);
-               player.add(warCard);
-            }
-         }//player won war
-         
-         else if (war_winner == 3)
+            game_over = 1;
+            System.out.println("Computer Won during War!");
+         }
+         else if (computer.isEmpty())
          {
-            for (int i = 0; i < warPileLength;i++)
-            {
-               warCard = warPile.remove(0);
-               computer.add(warCard);
-            }
-         }//computer won war
-         else 
+            game_over = 1;
+            System.out.println("Player Won during War!");
+         }
+         if (game_over == 0)
          {
-            if (war_var > 1) this.warRound();
+            warPile.add(temp_player_card); 
+            warPile.add(temp_computer_card);
             
-         }//must play another round
-         System.out.println(player.size());
-         System.out.println(computer.size());
-         war_var = 0;
-         return 0;
-      }//warRound method
+            //System.out.println(player.size());
+            //System.out.println(computer.size());
+            //System.out.println("WarPile Size: " +warPile.size());
+            
+            int war_winner = this.playRound(); //play a round to determine the winner.
+            
+            int warPileLength = warPile.size();
+            if (war_winner == 2)
+            {
+               for (int i = 0; i < warPileLength;i++)
+               {
+                  warCard = warPile.remove(0);
+                  player.add(warCard);
+               }
+            }//player won war
+            
+            else if (war_winner == 3)
+            {
+               for (int i = 0; i < warPileLength;i++)
+               {
+                  warCard = warPile.remove(0);
+                  computer.add(warCard);
+               }
+            }//computer won war
+            else 
+            {
+               if (war_var > 1) this.warRound();
+               
+            }//must play another round
+            System.out.println(player.size());
+            System.out.println(computer.size());
+            war_var = 0;
+            return 0;
+         }//game over is zero
+         return 5;
+     }//warRound method
    
-   public static void main(String[] args) throws IndexOutOfBoundsException
-   {
-       int blah = 1;
-
-      try
-      {
-         War warGame = new War();
-         Card testCard = warGame.player.get(0); //trying to figure out how to setImageIcon
-        
-         
-         do
-         {
-            blah = warGame.playRound();
-            System.out.println("Round: " + warGame.round_count);
-
-         } while(true);
-      } //try
-      catch (IndexOutOfBoundsException e)
-      {
-         System.out.println("Game Over");
-         if (blah==3) System.out.println("Computer Won");
-         if (blah==2) System.out.println("Player Won");
-         
-      }//catch
-      //warGame.warRound();
-
-      
-      //System.out.println("Round: " + warGame.round_count);
-   }//main
+//    public static void main(String[] args) throws IndexOutOfBoundsException
+//    {
+//        int blah = 1;
+// 
+//       try
+//       {
+//          War warGame = new War();
+//          Card testCard = warGame.player.get(0); //trying to figure out how to setImageIcon
+//         
+//          
+//          do
+//          {
+//             blah = warGame.playRound();
+//             System.out.println("Round: " + warGame.round_count);
+// 
+//          } while(true);
+//       } //try
+//       catch (IndexOutOfBoundsException e)
+//       {
+//          System.out.println("Game Over");
+//          if (blah==3) System.out.println("Computer Won");
+//          if (blah==2) System.out.println("Player Won");
+//          
+//       }//catch
+//       //warGame.warRound();
+// 
+//       
+//       //System.out.println("Round: " + warGame.round_count);
+//    }//main
 }//public class 
--- WarGUI.java
+++ WarGUI.java
@@ -4,12 +4,12 @@ import java.awt.event.*;
 
 public class WarGUI extends JFrame
 {
-   private War warGame;
-   private JPanel leftPanel= new JPanel(new GridLayout(3,1,1,4));
-   private JPanel rightPanel= new JPanel(new GridLayout(3,1,1,4));
-   private JPanel buttons = new JPanel(new GridLayout(4,1,2,2));
-   private JButton playButton, exit, newGameButton;
-   private JLabel status, title, per, com, round;
+   private War warGame; //warGame 
+   private JPanel leftPanel= new JPanel(new GridLayout(4,1,1,4)); //player info
+   private JPanel rightPanel= new JPanel(new GridLayout(4,1,1,4));//computer info
+   private JPanel buttons = new JPanel(new GridLayout(5,1,2,2)); //button panel
+   private JButton playButton, exit, newGameButton, finish;
+   private JLabel status, title, per, com, round, round_winner, pCC, cCC;
    private ImageIcon cardBack;
    
    private JLabel back1 = new JLabel();
@@ -29,14 +29,21 @@ public class WarGUI extends JFrame
       playButton =new JButton("Play a round!");
       newGameButton = new JButton("Play a new game...");
       exit = new JButton("Exit");
+      finish = new JButton("Play lots of rounds."); //finish game
       
       cardBack = new ImageIcon("back.jpg");
       
+      finish.addActionListener(new FinishButtonListener());//finish game
       playButton.addActionListener(new PlayButtonListener());
       newGameButton.addActionListener(new NewGameButtonListener());
       exit.addActionListener(new ExitHandler());
       
       round = new JLabel("Round: ");
+      round_winner = new JLabel(" ");
+      
+      pCC = new JLabel("Player Card Count: ");
+      cCC = new JLabel("Computer Card Count: ");
+      
       
       title = new JLabel("Nate's War Game"); 
       buttons.add(title);
@@ -52,6 +59,7 @@ public class WarGUI extends JFrame
       
       buttons.add(playButton);
       buttons.add(newGameButton);
+      buttons.add(finish);//finish game
       buttons.add(exit);
       this.getContentPane().add(buttons);
       pack();
@@ -60,6 +68,9 @@ public class WarGUI extends JFrame
       
       leftPanel.add(back1);
       rightPanel.add(back2);
+      
+      leftPanel.add(pCC);
+      rightPanel.add(cCC);
       
       add(leftPanel);
       add(rightPanel);
@@ -70,18 +81,88 @@ public class WarGUI extends JFrame
       
    }//war GUI
    
-   class PlayButtonListener implements ActionListener
+   class FinishButtonListener implements ActionListener 
+   {
+      public void actionPerformed(ActionEvent e) throws IndexOutOfBoundsException, NullPointerException
+      {     
+         if (e.getSource() instanceof JButton)
+         { 
+            int blah=0;
+            
+            do
+            {
+               blah = warGame.playRound();   
+            } while((warGame.player.size()>3) && (warGame.computer.size()>3));
+             //try
+               leftPanel.remove(pCC);
+               rightPanel.remove(cCC);
+               rightPanel.remove(round_winner);
+               leftPanel.remove(round);
+               rightPanel.remove(round_winner);
+               int winner = warGame.playRound();
+               Card testCard1 = warGame.player.get(0);
+               Card testCard2 = warGame.computer.get(0);
+               back1.setIcon(testCard1.getImageIcon());
+               back2.setIcon(testCard2.getImageIcon());
+               round = new JLabel("Round: " + warGame.round_count);               //JLabel game_over = new JLabel("Game Over");
+               //leftPanel.add(game_over);
+               int playerCardCount = warGame.player.size();
+               int computerCardCount = warGame.computer.size();
+            
+               pCC = new JLabel("\nPlayer Card Count: " + playerCardCount);
+               cCC = new JLabel("\nComputer Card Count: " + computerCardCount);
+            
+               leftPanel.add(pCC);
+               rightPanel.add(cCC);
+               leftPanel.add(round);
+      
+            //catch
+         }//if button source
+      }//actionPerformer
+   }//finish button listener
+   
+   
+   class PlayButtonListener implements ActionListener //plays a round of war
    {
       public void actionPerformed(ActionEvent e)
       {     
          if (e.getSource() instanceof JButton)
          {   
-            int winner = warGame.playRound();
+            leftPanel.remove(pCC); //remove all of the old information
+            rightPanel.remove(cCC);
+            leftPanel.remove(round);
+            rightPanel.remove(round_winner);
+            int winner = warGame.playRound(); 
             Card testCard1 = warGame.player.get(0);
             Card testCard2 = warGame.computer.get(0);
-            back1.setIcon(testCard1.getImageIcon());
+            back1.setIcon(testCard1.getImageIcon()); //flip 
             back2.setIcon(testCard2.getImageIcon());
             round = new JLabel("Round: " + warGame.round_count);
+            
+            
+            int playerCardCount = warGame.player.size();
+            int computerCardCount = warGame.computer.size();
+            
+            pCC = new JLabel("\nPlayer Card Count: " + playerCardCount); //display the number of cards player has
+            cCC = new JLabel("\nComputer Card Count: " + computerCardCount); //display the number of cards computer has
+            
+            leftPanel.add(pCC);
+            rightPanel.add(cCC);
+            
+            if (testCard1.getRank()>testCard2.getRank())
+            {
+               round_winner = new JLabel("Player won!");
+
+            }
+            else if (testCard1.getRank()<testCard2.getRank())
+            {
+               round_winner = new JLabel("Computer won!");
+            }
+            else
+            { 
+               round_winner = new JLabel("War!");
+            }
+            rightPanel.add(round_winner);
             leftPanel.add(round);
             
          } 
@@ -94,12 +175,28 @@ public class WarGUI extends JFrame
       {     
          if (e.getSource() instanceof JButton)
          {
-            War warGame = new War();
-            Card testCard1 = warGame.player.get(0);
-            Card testCard2 = warGame.computer.get(0);
-            back1.setIcon(testCard1.getImageIcon());
-            back2.setIcon(testCard2.getImageIcon());
+            leftPanel.remove(pCC);
+            rightPanel.remove(cCC);
+            leftPanel.remove(round);
+            rightPanel.remove(round_winner);
+            warGame = new War();
+            back2.setIcon(cardBack);
+            back1.setIcon(cardBack); 
+//             System.out.println(warGame.player.size());
+//             Card testCard1 = warGame.player.get(0);
+//             Card testCard2 = warGame.computer.get(0);
+//             back1.setIcon(testCard1.getImageIcon());
+//             back2.setIcon(testCard2.getImageIcon());
             
+            int playerCardCount = warGame.player.size();
+            int computerCardCount = warGame.computer.size();
+            
+            pCC = new JLabel("\nPlayer Card Count: " + playerCardCount);
+            cCC = new JLabel("\nComputer Card Count: " + computerCardCount);
+            
+            leftPanel.add(pCC);
+            rightPanel.add(cCC);
+
             
             //System.out.println(warGame.player.size());
             //System.out.println(warGame.computer.size());
